966dd09490c9c71ea03e7450821eff0e840c9b3e
AlZurn <alex.zurnachyan@vk.com>
2021-09-05 11:18:05 +0300

    Alex PARABMUNQ

---
+++ 1.py
@@ -0,0 +1,59 @@
+# x = int(input("num:"))
+# i = 0
+# for i in range(2,x):
+# 	if x % i == 0:
+# 		print(x,'нечет')
+# 		break
+# else:
+# 	print(x,'chet')
+
+
+# while True:
+# 	age = input('Your age: ')
+# 	if age.isdigit():
+# 		print(age)
+# 		break
+# 	else:
+# 		print("krkni")
+
+
+# x = float(input(':  '))
+# res = 32+1.8*x 2  10.5    3  14.5 4 18.5
+
+
+# age = int(input(': '))
+# res = 'mik gre bacasagan'
+# if age <= 2 and age >= 0:
+# 	res = 5.25*age
+# elif age > 2:
+# 	res = 10.5 + (age-2)*4
+# print(res)
+
+
+# x = input("as: ")
+# res = x == x.upper()
+ 
+
+
+# x = int(input(' :  '))
+# zy = x // 2
+# q = x - zy 
+
+
+# name = 'Alex'
+# print(f'hy {name}.')
+
+
+# x = int(input(' :'))
+# y = int(input(' :'))
+# cout = 0
+# for res in range (y,y*x+1):
+# 	cout += 1
+# 	if res % x == 0 and res % y == 0:
+# 		print(res,cout)
+# 		break
+# for i in'range':
+# 	for j in 'barev':
+# 		print(i,j)
+# x = 'barev'
+# print(x.[::-1])
