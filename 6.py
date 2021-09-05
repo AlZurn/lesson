b138faf984ec992b83e40a8bb94f8d7075040529
AlZurn <alex.zurnachyan@vk.com>
2021-09-05 11:26:21 +0300

    poxac

--- 6.py
+++ 6.py
@@ -1,10 +1,11 @@
-# l = [1,3,2,4,8,2,6,4,5,1]
-# my = []
+l = [1,3,2,4,8,2,6,4,5,1]
+my = []
+# COMEMMMENNNTTTTTTTTTTTTTTTTTTTTTTTTTT
 
-# for i in l:
-# 	if l.count(i) == 1:
-# 		my.append(i)
-# print(my)
+for i in l:
+	if l.count(i) == 1:
+		my.append(i)
+print(my)
 fruits = ['orange','apple', 'banana','banana', 'banana']
 price = [50, 20, 30, 30, 20]
 weight = [10, 2, 25, 25, 10]
@@ -17,9 +18,9 @@ for i in zip(fruits,price,weight):
 	else:
 		res.append(i)
 	
-# for i in res:
-# 	if res.count(i) > 1:
-# 		count += 1
+for i in res:
+	if res.count(i) > 1:
+		count += 1
 print(res)
 print(count)
 
